b314c78487ec405fda8c84a59f92415c6d14065c
David Bau <davidbau@csail.mit.edu>
2020-06-12 19:19:06 -0400

    Add some notebooks.

---
+++ notebooks/setup_notebooks.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Start from directory of script
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+# Set up git config filters so huge output of notebooks is not committed.
+git config filter.clean_ipynb.clean "$(pwd)/ipynb_drop_output.py"
+git config filter.clean_ipynb.smudge cat
+git config filter.clean_ipynb.required true
+
+# Set up symlinks for the example notebooks
+for DIRNAME in datasets
+do
+    mkdir -p ../${DIRNAME}
+    ln -sfn ../${DIRNAME} .
+done
