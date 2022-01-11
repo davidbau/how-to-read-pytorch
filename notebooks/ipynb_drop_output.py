05163935b9b32b6dd2bbcafa83a7fe57f3fefcc0
David Bau <david.bau@gmail.com>
2022-01-10 19:18:32 -0500

    Fix typo in comment.

--- notebooks/ipynb_drop_output.py
+++ notebooks/ipynb_drop_output.py
@@ -6,7 +6,9 @@ Suppress output and prompt numbers in git version control.
 This script will tell git to ignore prompt numbers and cell output
 when looking at ipynb files UNLESS their metadata contains:
 
-    "git" : { "keep_output" : true }
+    "git": {
+     "keep_outputs": true
+    },
 
 The notebooks themselves are not changed.
 
@@ -16,14 +18,14 @@ Usage instructions
 ==================
 
 1. Put this script in a directory that is on the system's path.
-   For future reference, I will assume you saved it in 
+   For future reference, I will assume you saved it in
    `~/scripts/ipynb_drop_output`.
 2. Make sure it is executable by typing the command
    `chmod +x ~/scripts/ipynb_drop_output`.
 3. Register a filter for ipython notebooks by
    putting the following line in `~/.config/git/attributes`:
    `*.ipynb  filter=clean_ipynb`
-4. Connect this script to the filter by running the following 
+4. Connect this script to the filter by running the following
    git commands:
 
    git config --global filter.clean_ipynb.clean ipynb_drop_output
@@ -54,7 +56,7 @@ Notes
 
 Changed by David Bau to make stripping output the default.
 
-This script is inspired by http://stackoverflow.com/a/20844506/827862, but 
+This script is inspired by http://stackoverflow.com/a/20844506/827862, but
 lets the user specify whether the ouptut of a notebook should be kept
 in the notebook's metadata, and works for IPython v3.0.
 """
@@ -72,10 +74,11 @@ if "git" in nb_metadata:
         keep_output = True
 if keep_output:
     sys.stdout.write(nb)
-    exit() 
+    exit()
 
 
-ipy_version = int(json_in["nbformat"])-1 # nbformat is 1 more than actual version.
+ipy_version = int(json_in["nbformat"]) - 1  # nbformat is 1 more than actual version.
+
 
 def strip_output_from_cell(cell):
     if "outputs" in cell:
@@ -94,4 +97,13 @@ else:
     for cell in json_in["cells"]:
         strip_output_from_cell(cell)
 
-json.dump(json_in, sys.stdout, sort_keys=True, indent=1, separators=(",",": "))
+json.dump(
+    json_in,
+    sys.stdout,
+    sort_keys=True,
+    indent=1,
+    separators=(",", ": "),
+    ensure_ascii=False,
+)
+# https://stackoverflow.com/questions/729692/why-should-text-files-end-with-a-newline
+sys.stdout.write("\n")
